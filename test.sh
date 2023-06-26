dea3ed4432e64f8e7f01b9bd598f8b3b9af46956
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-06-26 18:05:58 +0100

    First commit - skeleton project

---
+++ test.sh
@@ -0,0 +1,24 @@
+# Format, lint and test the entire project
+find . -name \*.py | entr -cs '
+	print_header () {
+		: "${COLUMNS:=79}"
+		set -- " $* "
+		while test ${#1} -lt $((COLUMNS - 1))
+		do
+			set -- "=${1}="
+		done
+		test ${#1} -lt $COLUMNS && set -- "${1}="
+		printf "\\e[1m%s\\e[0m\\n" "$1"  # Bold
+	}
+
+	set -e
+	. venv/bin/activate
+
+	print_header "Black"
+	black "$0"
+
+	print_header "Flake8"
+	flake8 "$0" && echo "No errors to display :)"
+
+	pytest --cov=. '"$*"'
+'
