b172851e9bdf0d036707c79667a32295f7a6f73a
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 20:41:33 +0100

    Refactor: Extract 'get_date' helper

--- superpy.py
+++ superpy.py
@@ -1,13 +1,18 @@
+def get_date():
+    try:
+        with open("superpy_date", "r") as date_file:
+            date = date_file.read()
+    except FileNotFoundError:
+        date = "1970-01-01"
+    return date
+
+
 def main(argv=None):
     if argv is None:
         return 1
     if len(argv) > 1:
         if argv[1] == "--advance":
-            try:
-                with open("superpy_date", "r") as date_file:
-                    date = date_file.read()
-            except FileNotFoundError:
-                date = "1970-01-01"
+            date = get_date()
             day = int(date[-2:])
             day += 1
             new_date = f"{date[:-2]}{day:02}"
@@ -17,10 +22,6 @@ def main(argv=None):
             with open("superpy_date", "w") as date_file:
                 date_file.write(argv[1])
     elif argv[0] == "date":
-        try:
-            with open("superpy_date", "r") as date_file:
-                date = date_file.read()
-        except FileNotFoundError:
-            date = "1970-01-01"
+        date = get_date()
         print(date)
     return 0
