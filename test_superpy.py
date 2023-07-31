fe3e9401b3b99da38a962fe99e0f7ffe846175d8
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 18:52:57 +0100

    Red: Does the application return 0 if arguments are passed?

--- test_superpy.py
+++ test_superpy.py
@@ -5,6 +5,10 @@ def test_returns_1_if_no_args():
     assert superpy.main() == 1
 
 
+def test_returns_0_if_args():
+    assert superpy.main(["frobble"]) == 0
+
+
 def test_date_without_args_prints_the_default_date(capsys):
     superpy.main(["date"])
     out, err = capsys.readouterr()
