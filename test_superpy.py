cf9c0cd312af90237971cd8bd8fce2b5cd042b09
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 18:45:29 +0100

    Red: Does 'superpy date' print the default date '1970-01-01\n'?

--- test_superpy.py
+++ test_superpy.py
@@ -3,3 +3,9 @@ import superpy
 
 def test_returns_1_if_no_args():
     assert superpy.main() == 1
+
+
+def test_date_without_args_prints_the_default_date(capsys):
+    superpy.main(["date"])
+    out, err = capsys.readouterr()
+    assert out == "1970-01-01\n"
