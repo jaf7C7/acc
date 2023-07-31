bc83b01fa2a4553ed01bca69e3f3c2317917fed5
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 22:33:15 +0100

    Red: Does 'parse_args' recognise 'date --advance 1'?

--- test_parse_args.py
+++ test_parse_args.py
@@ -6,8 +6,15 @@ import superpy
 @pytest.mark.parametrize(
     "args, expected",
     [
-        (["date"], argparse.Namespace(command="date", date=None)),
-        (["date", "2020-02-02"], argparse.Namespace(command="date", date="2020-02-02")),
+        (["date"], argparse.Namespace(command="date", date=None, days_to_advance=None)),
+        (
+            ["date", "2020-02-02"],
+            argparse.Namespace(command="date", date="2020-02-02", days_to_advance=None),
+        ),
+        (
+            ["date", "--advance", "1"],
+            argparse.Namespace(command="date", date=None, days_to_advance=1),
+        ),
     ],
 )
 def test_parse_args(args, expected):
