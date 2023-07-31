2d17833203ff89f3c58555dc6f3caad44cd507f4
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 22:25:55 +0100

    Red: Does 'parse_args' store the date to be set?

--- test_parse_args.py
+++ test_parse_args.py
@@ -6,7 +6,8 @@ import superpy
 @pytest.mark.parametrize(
     "args, expected",
     [
-        (["date"], argparse.Namespace(command="date")),
+        (["date"], argparse.Namespace(command="date", date=None)),
+        (["date", "2020-02-02"], argparse.Namespace(command="date", date="2020-02-02")),
     ],
 )
 def test_parse_args(args, expected):
