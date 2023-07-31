d471c341d6b1172eca4df0ba84a35d29a9f56bfc
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 19:12:17 +0100

    Red: Can the application get and set the date?

--- test_superpy.py
+++ test_superpy.py
@@ -1,6 +1,14 @@
 import os
-import pytest  # noqa: F401
+import pytest
 import superpy
+
+
+@pytest.fixture(autouse=True)
+def cleanup():
+    try:
+        os.unlink("superpy_date")
+    except FileNotFoundError:
+        pass
 
 
 def test_returns_1_if_no_args():
@@ -18,6 +26,7 @@ def test_date_without_args_prints_the_default_date(capsys):
 
 
 def test_can_set_and_get_the_date(capsys):
+<<<<<<< HEAD
     try:
         superpy.main(["date", "2020-02-02"])
         superpy.main(["date"])
@@ -39,3 +48,9 @@ def test_can_advance_date_by_days(capsys, days, expected):
         assert out == expected
     finally:
         os.unlink("superpy_date")
+=======
+    superpy.main(["date", "2020-02-02"])
+    superpy.main(["date"])
+    out, err = capsys.readouterr()
+    assert out == "2020-02-02\n"
+>>>>>>> 2c79268 (Red: Can the application get and set the date?)
