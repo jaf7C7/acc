f627c5d7369ed38a5dc64d6f186276010b9d4022
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 20:18:48 +0100

    Refactor: Remove unecessary fixture

--- test_superpy.py
+++ test_superpy.py
@@ -1,14 +1,6 @@
 import os
-import pytest
+import pytest  # noqa: F401
 import superpy
-
-
-@pytest.fixture(autouse=True)
-def cleanup():
-    try:
-        os.unlink("superpy_date")
-    except FileNotFoundError:
-        pass
 
 
 def test_returns_1_if_no_args():
@@ -30,3 +22,4 @@ def test_can_set_and_get_the_date(capsys):
     superpy.main(["date"])
     out, err = capsys.readouterr()
     assert out == "2020-02-02\n"
+    os.unlink("superpy_date")
