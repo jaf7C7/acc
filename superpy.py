59dec0df0d21c53a1fb1d88c1def73e76f04cdfe
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 20:38:02 +0100

    Green: 'date --advance 1' advances the date by 1 day

--- superpy.py
+++ superpy.py
@@ -2,8 +2,20 @@ def main(argv=None):
     if argv is None:
         return 1
     if len(argv) > 1:
-        with open("superpy_date", "w") as date_file:
-            date_file.write(argv[1])
+        if argv[1] == "--advance":
+            try:
+                with open("superpy_date", "r") as date_file:
+                    date = date_file.read()
+            except FileNotFoundError:
+                date = "1970-01-01"
+            day = int(date[-2:])
+            day += 1
+            new_date = f"{date[:-2]}{day:02}"
+            with open("superpy_date", "w") as date_file:
+                date_file.write(new_date)
+        else:
+            with open("superpy_date", "w") as date_file:
+                date_file.write(argv[1])
     elif argv[0] == "date":
         try:
             with open("superpy_date", "r") as date_file:
