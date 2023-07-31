1e35cdafc62857f51aaf9043262243fbc6aea06e
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 21:02:44 +0100

    Refactor: Make sure test files are always cleaned up

--- test_superpy.py
+++ test_superpy.py
@@ -18,11 +18,13 @@ def test_date_without_args_prints_the_default_date(capsys):
 
 
 def test_can_set_and_get_the_date(capsys):
-    superpy.main(["date", "2020-02-02"])
-    superpy.main(["date"])
-    out, err = capsys.readouterr()
-    assert out == "2020-02-02\n"
-    os.unlink("superpy_date")
+    try:
+        superpy.main(["date", "2020-02-02"])
+        superpy.main(["date"])
+        out, err = capsys.readouterr()
+        assert out == "2020-02-02\n"
+    finally:
+        os.unlink("superpy_date")
 
 
 @pytest.mark.parametrize(
