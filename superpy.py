a588b91235a03b65e371f41677bee1171cc82f39
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-07-31 19:35:10 +0100

    Green: Application can set and get the date

--- superpy.py
+++ superpy.py
@@ -1,6 +1,14 @@
 def main(argv=None):
     if argv is None:
         return 1
-    if argv[0] == "date":
-        print("1970-01-01")
+    if len(argv) > 1:
+        with open("superpy_date", "w") as date_file:
+            date_file.write(argv[1])
+    elif argv[0] == "date":
+        try:
+            with open("superpy_date", "r") as date_file:
+                date = date_file.read()
+        except FileNotFoundError:
+            date = "1970-01-01"
+        print(date)
     return 0
