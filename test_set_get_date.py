cfe6170931c2eca2fcca72f45c7ab1cf770089c8
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-08-01 18:42:47 +0100

    Red: Does 'set_config' write to the config file properly?

--- test_set_get_date.py
+++ test_set_get_date.py
@@ -9,3 +9,13 @@ def test_get_config():
         assert superpy.get_config() == dict(date="2000-01-01", ledger="/tmp/bar")
     finally:
         os.unlink(".superpy.conf")
+
+
+def test_set_config():
+    try:
+        superpy.set_config("date", "1664-08-17")
+        superpy.set_config("ledger", "/tmp/frobble")
+        with open(".superpy.conf", "r") as c:
+            assert c.read().split() == ["1664-08-17", "/tmp/frobble"]
+    finally:
+        os.unlink(".superpy.conf")
