7ca9d7091e4564fb9cd60869eed18f9425315809
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-08-22 23:32:58 +0100

    Remove unnecessary commands from test script

--- test.sh
+++ test.sh
@@ -1,26 +1,6 @@
 # Format, lint and test the entire project
-find . -name \*.py | entr -cs '
-	print_header () {
-		: "${COLUMNS:=79}"
-		set -- " $* "
-		while test ${#1} -lt $((COLUMNS - 1))
-		do
-			set -- "=${1}="
-		done
-		test ${#1} -lt $COLUMNS && set -- "${1}="
-		printf "\033[1m%s\033[0m\n" "$1"  # Bold
-	}
-
-	set -e
+find . -name \*superpy.py | entr -cs '
 	. venv/bin/activate
-
 	test -n "$TMUX_PANE" && tmux clear -t "$TMUX_PANE"
-
-	print_header "Black"
-	black --line-length 90 "$0"
-
-	print_header "Flake8"
-	flake8 "$0" && echo "No errors to display :)"
-
 	pytest --cov=. '"$*"'
 '
