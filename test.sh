ba46edcae2950a3f5d4890ccc9ef4f34a2a49116
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-08-24 18:11:42 +0100

    Add linting and formatting to test script

--- test.sh
+++ test.sh
@@ -1,6 +1,29 @@
 # Format, lint and test the entire project
-find . -name \*superpy.py | entr -cs '
+find . -name \*.py | entr -cs '
+	print_header () {
+		: "${COLUMNS:=79}"
+		set -- " $* "
+		while test ${#1} -lt $((COLUMNS - 1))
+		do
+			set -- "=${1}="
+		done
+		test ${#1} -lt $COLUMNS && set -- "${1}="
+		printf "\033[1m%s\033[0m\n" "$1"  # Bold
+	}
+
+	set -e
 	. venv/bin/activate
+
 	test -n "$TMUX_PANE" && tmux clear -t "$TMUX_PANE"
+
+	print_header "Black"
+	black --line-length 90 "$0"
+
+	print_header "Flake8"
+	flake8 "$0" && echo "No errors to display :)"
+
+    print_header "MyPy"
+    mypy "$0"
+
 	pytest --cov=. '"$*"'
 '
