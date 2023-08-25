ec48842c6f314dde192ef4c70e45e4d55a8077d9
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-08-25 16:12:18 +0100

    Move shared fixtures to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import csv
+import os
+import pytest
+
+CONFIG_PATH = ".acc.conf"
+LEDGER_PATH = "acc_ledger.csv"
+
+
+@pytest.fixture(autouse=True)
+def temp_dir(tmp_path):
+    os.chdir(tmp_path)
+    yield
+    for file in CONFIG_PATH, LEDGER_PATH:
+        try:
+            os.unlink(file)
+        except FileNotFoundError:
+            pass
+
+
+@pytest.fixture
+def mock_ledger():
+    ledger = [
+        ["id", "date", "amount", "type", "description"],
+        ["0", "1970-01-01", "2495.00", "credit", "frobulator"],
+        ["1", "1970-01-01", "5250.00", "debit", "frobulator"],
+    ]
+    with open(LEDGER_PATH, "w", newline="") as f:
+        writer = csv.writer(f)
+        for transaction in ledger:
+            writer.writerow(transaction)
--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -1,4 +1,3 @@
-import os
 import csv
 import datetime
 from decimal import Decimal
@@ -6,37 +5,12 @@ import pytest
 from acc import main
 
 
-CONFIG_PATH = ".acc.conf"
 LEDGER_PATH = "acc_ledger.csv"
-
-
-@pytest.fixture(autouse=True)
-def temp_dir(tmp_path):
-    os.chdir(tmp_path)
-    yield
-    for file in CONFIG_PATH, LEDGER_PATH:
-        try:
-            os.unlink(file)
-        except FileNotFoundError:
-            pass
 
 
 @pytest.fixture
 def ledger():
     return main.Ledger(LEDGER_PATH)
-
-
-@pytest.fixture
-def mock_ledger(self):
-    ledger = [
-        ["id", "date", "amount", "type", "description"],
-        ["0", "1970-01-01", "2495.00", "credit", "frobulator"],
-        ["1", "1970-01-01", "5250.00", "debit", "frobulator"],
-    ]
-    with open(LEDGER_PATH, "w", newline="") as f:
-        writer = csv.writer(f)
-        for transaction in ledger:
-            writer.writerow(transaction)
 
 
 def test_write(ledger):
