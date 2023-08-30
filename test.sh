4ea9238ec8e27850697e0cfd67d11e30cfb54d5e
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2023-08-30 14:26:25 +0100

    Add emacs-compatible tags

--- test.sh
+++ test.sh
@@ -1,31 +1,32 @@
 # Format, lint and test the entire project
 find src/ tests/ -name \*.py | entr -s '
-	print_header () {
-		: "${COLUMNS:=79}"
-		set -- " $* "
-		while test ${#1} -lt $((COLUMNS - 1)); do
-			set -- "=${1}="
-		done
-		test ${#1} -lt $COLUMNS && set -- "${1}="
-		printf "\033[1m%s\033[0m\n" "$1"  # Bold
-	}
+    print_header () {
+        : "${COLUMNS:=79}"
+        set -- " $* "
+        while test ${#1} -lt $((COLUMNS - 1)); do
+            set -- "=${1}="
+        done
+        test ${#1} -lt $COLUMNS && set -- "${1}="
+        printf "\033[1m%s\033[0m\n" "$1"  # Bold
+    }
 
-	set -e
-	. .venv/bin/activate
+    set -e
+    . .venv/bin/activate
 
     clear
-	test -n "$TMUX_PANE" && tmux clear -t "$TMUX_PANE"
+    test -n "$TMUX_PANE" && tmux clear -t "$TMUX_PANE"
 
-    find src/ tests/ -name \*.py -exec ctags {} +
+    find src/ tests/ -name \*.py -exec ctags {} +     # Vi-compatible
+    find src/ tests/ -name \*.py -exec ctags -e {} +  # Emacs-compatible
 
-	print_header "Black"
-	black --line-length 90 src/ tests/
+    print_header "Black"
+    black --line-length 90 src/ tests/
 
-	print_header "Flake8"
-	flake8 src/ tests/ && echo "No errors to display :)"
+    print_header "Flake8"
+    flake8 src/ tests/ && echo "No errors to display :)"
 
     print_header "MyPy"
     mypy src/ tests/
 
-	pytest --cov=src/ tests/ '"$*"'
+    pytest --cov=src/ tests/ '"$*"'
 '
